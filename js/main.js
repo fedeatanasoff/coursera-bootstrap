a7736209b96d86bb4c9c525ea8af3b04bb4c4a17
fedeatanasoff <fede.atanasoff@gmail.com>
2019-05-13 10:40:10 -0300

    agregando eventos al modal con jquery

--- js/main.js
+++ js/main.js
@@ -4,4 +4,24 @@ $(function() {
   $(".carousel").carousel({
     interval: 2500
   });
+
+  // MODAL
+  $("#exampleModal").on("show.bs.modal", function(e) {
+    console.log("abriendo modal");
+    $("#modalBtn").removeClass("btn-info");
+  });
+  $("#exampleModal").on("shown.bs.modal", function(e) {
+    console.log("modal abierto");
+    $("#modalBtn").addClass("btn-warning");
+    $("#modalBtn").prop("disabled", true);
+  });
+  $("#exampleModal").on("hide.bs.modal", function(e) {
+    console.log("escondiendo modal");
+    $("#modalBtn").removeClass("btn-warning");
+    $("#modalBtn").addClass("btn-info");
+  });
+  $("#exampleModal").on("hidden.bs.modal", function(e) {
+    console.log("modal escondido");
+    $("#modalBtn").prop("disabled", false);
+  });
 });
